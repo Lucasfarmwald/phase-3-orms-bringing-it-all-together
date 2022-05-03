7cf201b0ed9b321b8719c8a9ccbd8076872f94ff
lucasfarmwald <farmwaldlucas@gmail.com>
2022-05-03 16:47:32 -0500

    initial commit

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,29 @@
 class Dog
+    attr_accessor  :name, :breed, :id
 
+    def initialize(name:, breed:, id: nil)
+        @name = name
+        @breed = breed
+
+
+    end
+    def self.drop_table
+        sql = <<-SQL
+        DROP TABLE IF EXISTS dogs
+        SQL
+        DB[:conn].execute(sql)
+    end
+    def self.create_table
+        sql = <<-SQL
+        CREATE TABLE IF NOT EXISTS dogs(
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            breed Text
+        )
+        SQL
+        DB[:conn].execute(sql)
+
+    end
+
+    DB = {:conn => SQLite3::Database.new("db/dogs.db")}
 end
